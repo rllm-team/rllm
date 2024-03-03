38dba3f43d087b17f7b1163cd22ce3d772f3a8a2
biuboomc <quantumcgx@163.com>
2024-03-03 16:53:58 +0800

    adapt to changes

---
+++ examples/graphsage/cora/models.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+
+class NeighborAggregator(nn.Module):
+    def __init__(self, input_dim, output_dim,
+                 use_bias=False, aggr_method="mean"):
+        """Aggregate Node Neighbors
+
+        Args:
+            input_dim: Dimension of input features.
+            output_dim: Dimension of output features.
+            use_bias: Whether to use bias (default: {False}).
+            aggr_method: Method for aggregating neighbors (default: {mean}).
+        """
+        super(NeighborAggregator, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.use_bias = use_bias
+        self.aggr_method = aggr_method
+        self.weight = nn.Parameter(torch.Tensor(input_dim, output_dim))
+        if self.use_bias:
+            self.bias = nn.Parameter(torch.Tensor(self.output_dim))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight)
+        if self.use_bias:
+            init.zeros_(self.bias)
+
+    def forward(self, neighbor_feature):
+        if self.aggr_method == "mean":
+            aggr_neighbor = neighbor_feature.mean(dim=1)
+        elif self.aggr_method == "sum":
+            aggr_neighbor = neighbor_feature.sum(dim=1)
+        elif self.aggr_method == "max":
+            aggr_neighbor = neighbor_feature.max(dim=1)
+        else:
+            raise ValueError("Unknown aggr type, expected sum, max, or mean, b\
+                ut got {}".format(self.aggr_method))
+
+        neighbor_hidden = torch.matmul(aggr_neighbor, self.weight)
+        if self.use_bias:
+            neighbor_hidden += self.bias
+
+        return neighbor_hidden
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, aggr_method={}'.format(
+            self.input_dim, self.output_dim, self.aggr_method)
+
+
+class SageGCN(nn.Module):
+    def __init__(self, input_dim, hidden_dim,
+                 activation=F.relu,
+                 aggr_neighbor_method="mean",
+                 aggr_hidden_method="sum"):
+        """ 
+        SageGCN definition
+        Args:
+            input_dim: Dimension of input features.
+            hidden_dim: Dimension of hidden layer features.
+                When aggr_hidden_method=sum, the output dimension is hidden_dim.
+                When aggr_hidden_method=concat, the output dimension is hidden_dim*2.
+            activation: Activation function.
+            aggr_neighbor_method: Method for aggregating neighbor features, options include ["mean", "sum", "max"].
+            aggr_hidden_method: Method for updating node features, options include ["sum", "concat"].
+        """
+        super(SageGCN, self).__init__()
+        assert aggr_neighbor_method in ["mean", "sum", "max"]
+        assert aggr_hidden_method in ["sum", "concat"]
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.aggr_neighbor_method = aggr_neighbor_method
+        self.aggr_hidden_method = aggr_hidden_method
+        self.activation = activation
+        self.aggregator = NeighborAggregator(input_dim, hidden_dim,
+                                             aggr_method=aggr_neighbor_method)
+        self.weight = nn.Parameter(torch.Tensor(input_dim, hidden_dim))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight)
+
+    def forward(self, src_node_features, neighbor_node_features):
+        neighbor_hidden = self.aggregator(neighbor_node_features)
+        self_hidden = torch.matmul(src_node_features, self.weight)
+
+        if self.aggr_hidden_method == "sum":
+            hidden = self_hidden + neighbor_hidden
+        elif self.aggr_hidden_method == "concat":
+            hidden = torch.cat([self_hidden, neighbor_hidden], dim=1)
+        else:
+            raise ValueError("Expected sum or concat, got {}"
+                             .format(self.aggr_hidden))
+        if self.activation:
+            return self.activation(hidden)
+        else:
+            return hidden
+
+    def extra_repr(self):
+        output_dim = self.hidden_dim if self.aggr_hidden_method == "sum" \
+            else self.hidden_dim * 2
+        return 'in_features={}, out_features={}, aggr_hidden_method=\
+            {}'.format(self.input_dim, output_dim, self.aggr_hidden_method)
+
+
+class GraphSage(nn.Module):
+    def __init__(self, input_dim, hidden_dim,
+                 num_neighbors_list):
+        super(GraphSage, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.num_neighbors_list = num_neighbors_list
+        self.num_layers = len(num_neighbors_list)
+        self.gcn = nn.ModuleList()
+        self.gcn.append(SageGCN(input_dim, hidden_dim[0]))
+        for index in range(0, len(hidden_dim) - 2):
+            self.gcn.append(SageGCN(hidden_dim[index], hidden_dim[index+1]))
+        self.gcn.append(SageGCN(
+            hidden_dim[-2],
+            hidden_dim[-1],
+            activation=None))
+
+    def forward(self, node_features_list):
+        hidden = node_features_list
+        for i in range(self.num_layers):
+            next_hidden = []
+            gcn = self.gcn[i]
+            for hop in range(self.num_layers - i):
+                src_node_features = hidden[hop]
+                src_node_num = len(src_node_features)
+                neighbor_node_features = hidden[hop + 1] \
+                    .view((src_node_num, self.num_neighbors_list[hop], -1))
+                h = gcn(src_node_features, neighbor_node_features)
+                next_hidden.append(h)
+            hidden = next_hidden
+        return F.log_softmax(hidden[0], dim=1)
+
+    def extra_repr(self):
+        return 'in_features={}, num_neighbors_list={}'.format(
+            self.input_dim, self.num_neighbors_list)
