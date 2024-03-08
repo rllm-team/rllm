9efede907bb0824b120574acac3b935901ba6250
kseconds <132831199+kseconds@users.noreply.github.com>
2024-03-08 21:19:22 +0800

    Add files via upload

---
+++ examples/sgc/rel-movielens1m/classification/models.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from torch.nn import Module
+import torch.nn.functional as F
+import math
+
+class SGC(nn.Module):
+    """
+    A Simple PyTorch Implementation of Logistic Regression.
+    Assuming the features have been preprocessed with k-step graph propagation.
+    """
+    def __init__(self, nfeat, nclass):
+        super(SGC, self).__init__()
+
+        self.W = nn.Linear(nfeat, nclass)
+
+    def forward(self, x, adj):
+        return self.W(x)
