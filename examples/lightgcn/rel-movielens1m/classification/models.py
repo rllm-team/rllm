69f3a4cd68e1fceb98590e273100f5a183f3d1bb
Arrebol-logos <1173851940@qq.com>
2024-03-09 19:27:45 +0800

    modify the explanation

---
+++ examples/lightgcn/rel-movielens1m/classification/models.py
@@ -0,0 +1,26 @@
+import sys
+import os.path as osp
+current_path = osp.dirname(__file__)
+sys.path.append(current_path + '/../../')
+from lightGCNconv import GraphConvolution
+
+import torch.nn as nn
+import torch.nn.functional as F
+from lightGCNconv import GraphConvolution
+
+class lightGCN(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(lightGCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nhid)
+        self.gc3 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc2(x, adj))   
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc3(x, adj) 
+        return F.logsigmoid(x)
