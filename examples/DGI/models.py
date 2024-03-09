06924b90f946cba83894da516ed11aa222a2e289
01WarpDrive <yixinzhou1024@sjtu.edu.cn>
2024-03-09 20:17:47 +0800

    add regression
    add regression

--- examples/DGI/models.py
+++ examples/DGI/models.py
@@ -50,3 +50,32 @@ class LogReg(nn.Module):
     def forward(self, seq):
         ret = self.fc(seq)
         return ret
+
+
+class Decoder(nn.Module):
+    def __init__(self, nhid):
+        super(Decoder, self).__init__()
+        self.lin1 = nn.Linear(2 * nhid, nhid)
+        self.lin2 = nn.Linear(nhid, 1)
+
+    def forward(self, z, adj):
+        row, col = adj.indices()[0], adj.indices()[1]
+        # print(z[col[:2]])
+        # print(z[4000:4002])
+        # z = (z[row] * z[col]).sum(dim=1)
+
+        z = torch.cat([z[row], z[col]], dim=-1)
+        z = self.lin1(z).relu()
+        z = self.lin2(z)
+        return z.view(-1)
+
+
+class Model(nn.Module):
+    def __init__(self, ft_in, nhid):
+        super(Model, self).__init__()
+        self.encoder = LogReg(ft_in, nhid)
+        self.decoder = Decoder(nhid)
+
+    def forward(self, seq, adj):
+        z_all = self.encoder(seq)
+        return self.decoder(z_all, adj)
