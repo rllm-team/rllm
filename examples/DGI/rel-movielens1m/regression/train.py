a08c8584e1d09f5221f9315b019b14e0aacee2e9
01WarpDrive <yixinzhou1024@sjtu.edu.cn>
2024-03-09 20:17:47 +0800

    add regression
    add regression

--- examples/DGI/rel-movielens1m/regression/train.py
+++ examples/DGI/rel-movielens1m/regression/train.py
@@ -1,8 +1,8 @@
-# Deep Graph Infomax (DGI) for classification task in rel-movielens1m
+# Deep Graph Infomax (DGI) for regression task in rel-movielens1m
 # Paper: Deep Graph Infomax (ICLR 2019)
 # Arxiv: https://arxiv.org/abs/1809.10341
-# Test f1_score micro: 0.2512, macro: 0.1295
-# Runtime: 66.4504s on a 6GB GPU (NVIDIA GeForce RTX 3060 laptop GPU)
+# Test MSE Loss: 1.3718
+# Runtime: 1737.6395s on a 6GB GPU (NVIDIA GeForce RTX 3060 laptop GPU)
 # Cost: N/A
 # Usage: python train.py
 
@@ -16,12 +16,10 @@ import torch.nn as nn
 import numpy as np
 import argparse
 import time
-from sklearn.metrics import f1_score
 
 # from utils import load_data function
 from load_data import load_data
-from examples.DGI.models import DGI, LogReg
-
+from examples.DGI.models import DGI, Model
 
 # Training settings
 parser = argparse.ArgumentParser()
@@ -30,13 +28,17 @@ parser.add_argument('--no_cuda', action='store_true',
 parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=72, help='Random seed.')
-parser.add_argument('--pre_epochs', type=int, default=200,
+parser.add_argument('--pre_epochs', type=int, default=60,
                     help='Number of epochs to pre_train.')
 parser.add_argument('--epochs', type=int, default=100,
                     help='Number of epochs to train.')
 parser.add_argument('--lr', type=float, default=0.001,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=0.0,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--gcn_lr', type=float, default=0.01,
+                    help='Initial learning rate.')
+parser.add_argument('--gcn_weight_decay', type=float, default=5e-4,
                     help='Weight decay (L2 loss on parameters).')
 parser.add_argument('--hidden', type=int, default=512,
                     help='Number of hidden units.')
@@ -59,16 +61,12 @@ if args.cuda:
     print("cuda")
 
 # Load data
-data, adj, features, labels, idx_train, idx_val, idx_test = load_data('movielens-classification', device=device)
+data, adj, features, labels, idx_train, idx_val, idx_test = load_data('movielens-regression', device=device)
 # sp_adj = process.load_data_adj_cora(device)
 nb_nodes = features.shape[0]
 ft_size = features.shape[1]
-nb_classes = labels.shape[1]
+
 features = features[np.newaxis]
-labels_train = labels.cpu()[idx_train.cpu()]
-labels_val = labels.cpu()[idx_val.cpu()]
-labels_test = labels.cpu()[idx_test.cpu()]
-labels = labels[np.newaxis]
 sp_adj = adj
 
 # Model and optimizer
@@ -76,12 +74,13 @@ model = DGI(ft_size, args.hidden, args.nonlinearity).to(device)
 optimizer = torch.optim.Adam(model.parameters(),
                              lr=args.lr, weight_decay=args.weight_decay)
 
-log = LogReg(args.hidden, nb_classes).to(device)
-opt = torch.optim.Adam(log.parameters(), lr=0.01, weight_decay=0.0)
-
+reg_model = Model(args.hidden, args.hidden).to(device)
+opt = torch.optim.Adam(reg_model.parameters(),
+                       lr=args.lr, weight_decay=args.weight_decay)
 
 b_xent = nn.BCEWithLogitsLoss()
 xent = nn.CrossEntropyLoss()
+loss_func = nn.MSELoss()
 
 
 def pre_train(pre_epoch):
@@ -114,64 +113,46 @@ def get_embed():
     model.eval()
     embeds, _ = model.embed(features, sp_adj if args.sparse else adj,
                             args.sparse, None)
-    train_embs = embeds[0, idx_train]
-    train_lbls = torch.argmax(labels[0, idx_train], dim=1)
-    val_embs = embeds[0, idx_val]
-    val_lbls = torch.argmax(labels[0, idx_val], dim=1)
-    test_embs = embeds[0, idx_test]
-    test_lbls = torch.argmax(labels[0, idx_test], dim=1)
-    return train_embs, train_lbls, val_embs, val_lbls, test_embs, test_lbls
+    return embeds[0]
 
 
 def train(epoch):
     t = time.time()
-    log.train()
+    reg_model.train()
     opt.zero_grad()
-    logits = log(train_embs)
-    pred = np.where(logits.cpu() > -1.0, 1, 0)
-
-    loss_train = xent(logits, train_lbls)
+    output = reg_model(embeds, adj)
+    loss_train = loss_func(output[idx_train], labels[idx_train])
     loss_train.backward()
-    f1_micro_train = f1_score(labels_train, pred, average="micro")
-    f1_macro_train = f1_score(labels_train, pred, average="macro")
     opt.step()
 
     if not args.fastmode:
         # Evaluate validation set performance separately,
         # deactivates dropout during validation run.
-        log.eval()
-        logits = log(val_embs)
+        reg_model.eval()
+        output = reg_model(embeds, adj)
 
-    loss_val = xent(logits, val_lbls)
-    f1_micro_val = f1_score(labels_val, pred, average="micro")
-    f1_macro_val = f1_score(labels_val, pred, average="macro")
-
+    loss_val = loss_func(output[idx_val], labels[idx_val])
     print('Epoch: {:04d}'.format(epoch+1),
           'loss_train: {:.4f}'.format(loss_train.item()),
-          'f1_train: {:.4f} {:.4f}'.format(f1_micro_train, f1_macro_train),
           'loss_val: {:.4f}'.format(loss_val.item()),
-          'f1_val: {:.4f} {:.4f}'.format(f1_micro_val, f1_macro_val),
           'time: {:.4f}s'.format(time.time() - t))
 
 
 def test():
-    log.eval()
-    logits = log(test_embs)
-    pred = np.where(logits.cpu() > -1.0, 1, 0)
-    loss_test = xent(logits, test_lbls)
-    f1_micro_test = f1_score(labels_test, pred, average="micro")
-    f1_macro_test = f1_score(labels_test, pred, average="macro")
+    reg_model.eval()
+    output = reg_model(embeds, adj)
+    loss_test = loss_func(output[idx_test], labels[idx_test])
     print("Test set results:",
-          "loss= {:.4f}".format(loss_test.item()),
-          "f1_test= {:.4f} {:.4f}".format(f1_micro_test, f1_macro_test))
+          "loss= {:.4f}".format(loss_test.item()))
 
 
 # Train model
 t_total = time.time()
+# args.pre_epochs
 for pre_epoch in range(args.pre_epochs):
     pre_train(pre_epoch)
 print("Pre_train Finished!")
-train_embs, train_lbls, val_embs, val_lbls, test_embs, test_lbls = get_embed()
+embeds = get_embed()
 for epoch in range(args.epochs):
     train(epoch)
 print("Optimization Finished!")
