55d1e4f480df8a83942168183fe8a4a76e279c8c
yanjiashuo <59167722+yanjiashuo@users.noreply.github.com>
2024-08-10 09:21:53 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,7 +96,7 @@ html_show_sourcelink = False
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = [
-    'custom.css',
+    'static/custom.css',
 ]
 
 
