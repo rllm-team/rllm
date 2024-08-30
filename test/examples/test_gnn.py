a8a0fa87e95bbb749bb59530bc2f9f287fc3aecb
z3u5 <1592050303@qq.com>
2024-08-30 13:13:59 +0800

    update gat

--- test/examples/test_gnn.py
+++ test/examples/test_gnn.py
@@ -26,7 +26,7 @@ def test_gat():
         out.returncode == 0
     ), f"stdout: {out.stdout.decode('utf-8')}\nstderr: {out.stderr.decode('utf-8')}"
     stdout = out.stdout.decode("utf-8")
-    assert float(stdout[-9:]) > 0.75
+    assert float(stdout[-9:]) > 0.82
 
 
 def test_han():
