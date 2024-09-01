2be617e6d514bbddfd94db2eb9f019a6df1a2e2a
yanjiashuo <59167722+yanjiashuo@users.noreply.github.com>
2024-09-01 22:09:17 +0800

    Update neighbor_sampler.py

--- rllm/sampler/neighbor_sampler.py
+++ rllm/sampler/neighbor_sampler.py
@@ -3,9 +3,6 @@ from typing import List, Optional
 
 import torch
 from torch import Tensor
-#from torch_sparse import SparseTensor
-
-#from rllm.utils.sparse import is_torch_sparse_tensor
 
 
 class NeighborSampler(torch.utils.data.DataLoader):
