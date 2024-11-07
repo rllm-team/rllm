f9985d08c650cf0689e9dad5395beaaa44ada2ec
z3u5 <1592050303@qq.com>
2024-11-07 14:08:58 +0800

    update test

--- test/examples/test_tnn.py
+++ test/examples/test_tnn.py
@@ -29,8 +29,18 @@ def test_tab_transformer():
     assert float(stdout[-9:]) > 0.80
 
 
-def test_tabnet():
+def test_excel_former():
     script = os.path.join(EXAMPLE_ROOT, "tabnet.py")
+    out = subprocess.run(["python", str(script)], capture_output=True)
+    assert (
+        out.returncode == 0
+    ), f"stdout: {out.stdout.decode('utf-8')}\nstderr: {out.stderr.decode('utf-8')}"
+    stdout = out.stdout.decode("utf-8")
+    assert float(stdout[-9:]) > 0.79
+
+
+def test_tabnet():
+    script = os.path.join(EXAMPLE_ROOT, "excelformer.py")
     out = subprocess.run(["python", str(script)], capture_output=True)
     assert (
         out.returncode == 0
