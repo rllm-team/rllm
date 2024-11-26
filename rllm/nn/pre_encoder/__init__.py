5e2277da50b6f132795ab64198ba8045e377184e
z3u5 <1592050303@qq.com>
2024-11-26 17:42:55 +0800

    delete nn init file

--- rllm/nn/pre_encoder/__init__.py
+++ rllm/nn/pre_encoder/__init__.py
@@ -1,11 +1,7 @@
-# from .coltype_encoders import (
-#     EmbeddingEncoder,
-#     LinearEncoder,
-#     StackEncoder,
-# )
 from .embedding_encoder import EmbeddingEncoder
 from .linear_encoder import LinearEncoder
 from .stack_encoder import StackEncoder
+
 __all__ = [
     "EmbeddingEncoder",
     "LinearEncoder",
