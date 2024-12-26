1fc5b45c33102dfa9879a2bf03678e383000e602
J1an5 <1592050303@qq.com>
2024-12-26 15:26:13 +0800

    Refactor parameter names for consistency

--- test/nn/conv/test_ft_transformer_convs.py
+++ test/nn/conv/test_ft_transformer_convs.py
@@ -1,12 +1,15 @@
 import torch
 
-from rllm.nn.conv.table_conv import FTTransformerConvs
+from rllm.nn.conv.table_conv import FTTransformerConv
 
 
 def test_ft_transformer_convs():
     x = torch.randn(size=(10, 3, 8))
-    conv = FTTransformerConvs(dim=8, layers=3)
-    x, x_cls = conv(x)
-    # The first added column corresponds to CLS token.
+    conv = FTTransformerConv(dim=8)
+    x = conv(x)
     assert x.shape == (10, 3, 8)
+
+    conv_cls = FTTransformerConv(dim=8, use_cls=True)
+    x_cls = conv_cls(x)
+    # The first added column corresponds to CLS token.
     assert x_cls.shape == (10, 8)
