e5dc71fdbc80e57870ee01205367419825f0ea46
J1an5 <1592050303@qq.com>
2025-04-10 21:57:10 +0800

    Modify bank_marketing url.

--- rllm/utils/extract.py
+++ rllm/utils/extract.py
@@ -3,5 +3,5 @@ import zipfile
 
 def extract_zip(path: str, output_folder: str):
     r"""Extract a zip archive to a specific folder."""
-    with zipfile.ZipFile(path, 'r') as f:
+    with zipfile.ZipFile(path, "r") as f:
         f.extractall(output_folder)
