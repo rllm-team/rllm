45c862fa893fa10d41c897818cb2bdd379613cad
p27 <prphrl27@sjtu.edu.cn>
2025-07-06 21:12:47 +0800

    Update doc configurations.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,7 +66,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
+autodoc_mock_imports = ["feat_engineer", "retriever"]
 
 # -- Options for HTML output -------------------------------------------------
 
