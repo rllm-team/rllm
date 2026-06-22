6d729eda5018b98fe7985dd9de28e458f27966d7
Ken Zhong <zhongken@bytedance.com>
2026-06-22 12:55:35 +0800

    add func docstring

--- rllm/utils/xavier_init.py
+++ rllm/utils/xavier_init.py
@@ -10,6 +10,13 @@ def _xavier_uniform_(tensor: Tensor, gain: float = 1.0) -> Tensor:
     Unlike :func:`torch.nn.init.xavier_uniform_`, this supports parameters with
     a leading group dimension (e.g. :obj:`[num_metapaths, cin, cout]`) by using
     the trailing two dimensions to compute the fan-in/fan-out.
+
+    Args:
+        tensor (Tensor): Parameter to be initialized.
+        gain (float): Gain factor. (default: :obj:`1.0`)
+        
+    Returns:
+        Tensor: The initialized parameter.
     """
     fan_in, fan_out = tensor.size()[-2:]
     std = gain * math.sqrt(2.0 / float(fan_in + fan_out))
